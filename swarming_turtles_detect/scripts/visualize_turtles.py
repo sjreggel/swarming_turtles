e3a333c42bc85f6276d8f76d61896a974208c5c9
sl04 <devnull@localhost>
2013-09-02 17:51:50 +0200

    detection works with visualizaiotn

--- swarming_turtles_detect/scripts/visualize_turtles.py
+++ swarming_turtles_detect/scripts/visualize_turtles.py
@@ -2,16 +2,123 @@
 
 
 import rospy
-
+import tf
+import math
+import copy
 from swarming_turtles_msgs.msg import Turtle,Turtles
+from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker, MarkerArray
 
+pub = None
+pub_me = None
+
+RADIUS = 0.17
 
 
+
+def cb_found_turtles(msg):
+    marker_array = MarkerArray()
+    count = 0
+    for turtle in msg.turtles:
+        add_markers(marker_array, turtle)
+    pub.publish(marker_array)
+    publish_me()
+
+    
+def add_markers(marker_array, turtle):
+    marker = Marker()
+    marker.header.frame_id = turtle.position.header.frame_id
+    marker.header.stamp = turtle.position.header.stamp
+    marker.ns = "base_frame"
+    marker.action = Marker.ADD
+    marker.type = Marker.SPHERE
+    marker.scale.x = 2 * RADIUS
+    marker.scale.y = 2 * RADIUS
+    marker.scale.z = 0.1
+    marker.color.r = 1.0
+    marker.color.a = 1.0
+    marker.id = len(marker_array.markers)
+    marker.lifetime = rospy.Duration(1)
+    marker.pose = turtle.position.pose
+    marker_array.markers.append(marker)
+
+
+
+    marker = Marker()
+    marker.header.frame_id = turtle.position.header.frame_id
+    marker.header.stamp = turtle.position.header.stamp
+    marker.ns = "base_frame"
+    marker.action = Marker.ADD
+    marker.type = Marker.TEXT_VIEW_FACING
+    marker.text = turtle.name
+    marker.scale.z = 0.3
+    marker.color.r = 1.0
+    marker.color.g = 1.0
+    marker.color.b = 1.0
+    marker.color.a = 1.0
+    marker.id = len(marker_array.markers)
+    marker.lifetime = rospy.Duration(1)
+    marker.pose = copy.deepcopy(turtle.position.pose)
+
+    marker.pose.position.z = 0.2
+
+    marker_array.markers.append(marker)
+
+    
+    marker = Marker()
+    marker.lifetime = rospy.Duration(1)
+
+    marker.header.frame_id = turtle.position.header.frame_id
+    marker.header.stamp = turtle.position.header.stamp
+    marker.ns = "base_frame"
+    marker.action = Marker.ADD
+    marker.type = Marker.ARROW
+    marker.scale.x = 0.1
+    marker.scale.y = 0.2
+    marker.scale.z = 0.1
+    marker.color.r = 1.0
+    marker.color.a = 1.0
+    marker.id = len(marker_array.markers)  
+    quat = turtle.position.pose.orientation
+    q = [quat.x, quat.y, quat.z, quat.w]
+    r,p,theta = tf.transformations.euler_from_quaternion(q)
+    
+    p = Point()
+    p = turtle.position.pose.position
+
+    marker.points.append(p)
+    
+    p = Point()
+    p.x = turtle.position.pose.position.x + 2 * RADIUS * math.cos(theta)
+    p.y = turtle.position.pose.position.y + 2 * RADIUS * math.sin(theta)
+    marker.points.append(p)
+    
+    marker_array.markers.append(marker)
+
+    
+def publish_me():
+    turtle = Turtle()
+    turtle.name  = "me"
+    turtle.position.header.frame_id = '/base_link'
+    turtle.position.header.stamp = rospy.Time.now()
+    turtle.position.pose.orientation.w = 1.0
+
+    marker_array = MarkerArray()
+
+    add_markers(marker_array, turtle)
+
+    pub_me.publish(marker_array)
+    
+    
+    
 def main():
+    global pub, pub_me
     rospy.init_node("visualize_turtles")
     
-
+    rospy.Subscriber("found_turtles", Turtles, cb_found_turtles)
+    pub = rospy.Publisher("neighbors", MarkerArray)
+    pub_me = rospy.Publisher("me", MarkerArray)
+    
     rospy.spin()
 
 
