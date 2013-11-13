03696bf9ff6d1ea98468c4039ceb1d5d7acb7f93
danielclaes <devnull@localhost>
2013-11-13 16:03:51 +0100

    lcm

---
+++ swarming_turtles_communicate/src/swarming_turtles_communicate/lcm_relay.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import rospy
+from swarming_turtles_msgs.msg import CommunicationProtocol
+
+from socket import gethostname
+
+import lcm
+import time
+import StringIO
+
+#from msg import communication_msg
+
+lc = None
+topic = '/communication'
+name = None
+lcm_sub = None
+pub = None
+
+def init_globals():
+    global lc, name, pub, lcm_sub
+    lc = lcm.LCM("udpm://224.1.1.1:5007?ttl=2")
+    name = gethostname()
+    lcm_sub = lc.subscribe(name, udp_callback)
+    pub = rospy.Publisher(topic, CommunicationProtocol)
+    rospy.Subscriber(topic, CommunicationProtocol, handle_msg)
+
+def udp_callback(channel, data):
+    #udp_msg = communication_msg.decode(data)
+    msg = CommunicationProtocol()
+    msg.deserialize(data)
+    pub.publish(msg)
+    
+def handle_msg(msg):
+    if msg.receiver == name:
+        return
+    send(msg)
+    
+def main():
+    rospy.init_node('swarming_turtles_machine')
+    init_globals()
+    
+    while not rospy.is_shutdown():
+        lc.handle()
+    lc.unsubscribe(lcm_sub)
+
+
+
+def send(msg, repeats = 3):
+    #send = communication_msg()
+    #print send
+    buff = StringIO.StringIO()
+    msg.serialize(buff)
+    print buff.getvalue()
+    #send.buffer = buff.getvalue()
+    for i in xrange(repeats):
+        lc.publish(msg.receiver, buff.getvalue())
+    
+    
+
+if __name__ == '__main__':
+    main()
