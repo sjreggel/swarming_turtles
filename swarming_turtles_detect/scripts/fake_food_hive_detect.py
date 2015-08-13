7042a73606cd534739f169f348b9ec162fb921d5
Daniel Claes <danielclaes@web.de>
2015-08-13 19:57:33 +0200

    view angle and distance for hive and food

--- swarming_turtles_detect/scripts/fake_food_hive_detect.py
+++ swarming_turtles_detect/scripts/fake_food_hive_detect.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+import math
+
 __author__ = 'danielclaes'
 import rospy
 import tf
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Vector3
 from swarming_turtles_detect.srv import *
 from nav_msgs.msg import Odometry
 
@@ -11,7 +13,14 @@ FOOD_TOPICS = ['/food/base_pose_ground_truth']
 
 WORLD_FRAME = '/world'
 HIVE_FRAME = rospy.get_param('hive_frame', '/hive')
+BASE_FRAME = rospy.get_param('base_frame', '/base_link')
 
+SEEN_FOOD = [False] * len(FOOD_TOPICS)
+SEEN_HIVE = False
+
+SEEN_DIST = 2.0
+
+SEEN_ANG = math.pi / 4
 
 RATE = 20
 
@@ -21,6 +30,17 @@ transform = {}  # transform of world to hive
 def quat_msg_to_array(quat):
     return [quat.x, quat.y, quat.z, quat.w]
 
+
+def dist_vec(a, b):
+    d = diff_vec(a, b)
+    return math.sqrt(d.x * d.x + d.y * d.y)
+
+
+def diff_vec(a, b):
+    res = Vector3()
+    res.x = b.x - a.x
+    res.y = b.y - a.y
+    return res
 
 class FakeFoodHiveDetect(object):
     def __init__(self):
@@ -35,7 +55,18 @@ class FakeFoodHiveDetect(object):
         rospy.Service('get_location', GetLocation, self.get_food)
         rospy.Service('forget_location', ForgetLocation, self.forget_food_location)
 
+    def seen_loc(self, pos):
+        res = self.transform_pose(pos, BASE_FRAME)
+        if res is not None:
+            if abs(dist_vec(res.pose.position, Vector3())) < SEEN_DIST:
+                rospy.logdebug("Location is in ViewDistance!")
+                if abs(math.atan2(res.pose.position.y, res.pose.position.x)) < SEEN_ANG:
+                    rospy.logdebug("Location is in ViewAngle!")
+                    return True
+        return False
+
     def food_cb(self, msg, idx):
+        global SEEN_FOOD
         """
         :param msg:
         :type msg: Odometry
@@ -49,6 +80,8 @@ class FakeFoodHiveDetect(object):
         p = self.transform_pose(p, HIVE_FRAME, time_in=msg.header.stamp)
         if p is not None:
             self.food_locations[key] = p
+            if not SEEN_FOOD[idx] and self.seen_loc(p):
+                SEEN_FOOD[idx] = True
 
     def transform_pose(self, pose_in, frame, time_in=None):
         if self.tf.frameExists(pose_in.header.frame_id) and self.tf.frameExists(frame):
@@ -69,7 +102,7 @@ class FakeFoodHiveDetect(object):
 
 
     def hive_cb(self, msg):
-        global transform
+        global transform, SEEN_HIVE
         """
         :param msg:
         :type msg: Odometry
@@ -79,7 +112,8 @@ class FakeFoodHiveDetect(object):
         transform['pose'] = (self.hive_location.pose.position.x, self.hive_location.pose.position.y, 0)
         transform['quat'] = tuple(quat_msg_to_array(self.hive_location.pose.orientation))
         transform['stamp'] = rospy.Time.now()
-
+        if not SEEN_HIVE and self.seen_loc(self.hive_location):
+            SEEN_HIVE = True
 
     def get_hive(self, req):
         """
@@ -89,8 +123,9 @@ class FakeFoodHiveDetect(object):
         :rtype: GetLocationResponse
         """
         res = GetLocationResponse()
-        res.res = "last_seen"
-        res.pose = self.hive_location
+        if SEEN_HIVE:
+            res.res = "last_seen"
+            res.pose = self.hive_location
         return res
 
     def get_food(self, req):
@@ -103,17 +138,20 @@ class FakeFoodHiveDetect(object):
         res = GetLocationResponse()
         if req.location == '':
             for l in self.food_locations.keys():
-                if self.food_locations[l] is not None:
+                idx = int(l[-1:])
+                if self.food_locations[l] is not None and SEEN_FOOD[idx]:
                     res.res = l
                     res.pose = self.food_locations[l]
                     break
         else:
-            if req.location in self.food_locations.keys():
+            idx = int(req.location[-1:])
+            if req.location in self.food_locations.keys() and SEEN_FOOD[idx]:
                 res.res = req.location
                 res.pose = self.food_locations[req.location]
         return res
 
     def forget_food_location(self, req):
+        global SEEN_FOOD
         """
         :param req:
         :type req: ForgetLocationRequest
@@ -122,9 +160,12 @@ class FakeFoodHiveDetect(object):
         """
         if req.location == '':
             self.food_locations = {}
+            SEEN_FOOD = [False] * len(FOOD_TOPICS)
         else:
             if req.location in self.food_locations.keys():
+                idx = int(req.location[-1:])
                 self.food_locations[req.location] = None
+                SEEN_FOOD[idx] = False
         return ForgetLocationResponse()
 
 
