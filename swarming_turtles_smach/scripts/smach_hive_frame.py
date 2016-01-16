4d27f7d5dbf9112fac8c14fd56945285eaaf9a55
Sjreggel <sjreggel@gmail.com>
2016-01-16 13:58:35 +0100

    one launchfile

--- swarming_turtles_smach/scripts/smach_hive_frame.py
+++ swarming_turtles_smach/scripts/smach_hive_frame.py
@@ -126,6 +126,22 @@ def init_globals():
     rospy.Subscriber('stall', Stall, cb_stall)  # turtle in collision?
 
 
+def talker():
+     pub = rospy.Publisher('chatter', String, queue_size=10)
+     rospy.init_node('talker', anonymous=True)
+     rate = rospy.Rate(10) # 10hz
+     while not rospy.is_shutdown():
+         hello_str = "hello world %s" % rospy.get_time()
+         rospy.loginfo(hello_str)
+         pub.publish(hello_str)
+         rate.sleep()
+ 
+if __name__ == '__main__':
+     try:
+         talker()
+     except rospy.ROSInterruptException:
+         pass
+
 def seen_hive():
     global get_hive_srv
     try:
